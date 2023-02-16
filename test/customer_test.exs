2c7ec7ac9e11d25376eea236dfede660a084b17b
singlefleck <singlefleck@gmx.com>
2023-02-16 14:55:31 +0530

    Updated customer test cases have been updated as for the reviewer

--- test/customer_test.exs
+++ test/customer_test.exs
@@ -1,50 +1,45 @@
-defmodule CustomerTest do
+defmodule Registration.CustomerTest do
   use ExUnit.Case
-  doctest Customer
+  doctest Registration.Customer
 
   setup_all do
     valid_customer = %{name: "boy", aadhar: "561664075050"}
     invalid_customer= %{}
 
     {:ok, valid_customer: valid_customer, invalid_customer: invalid_customer}
-
-
   end
 
   test "valid customer", state do
-    assert Customer.is_valid(state[:valid_customer])
+    assert Registration.Customer.is_valid(state[:valid_customer])
   end
 
   test "invalid customer", state do
-    refute Customer.is_valid(state[:invalid_customer])
+    refute Registration.Customer.is_valid(state[:invalid_customer])
   end
 
   test "invalid by name", state do
     valid_customer = state[:valid_customer]
 
     invalid_customer_by_name = %{valid_customer | name: "ra"}
-    refute Customer.is_valid(invalid_customer_by_name)
+    refute Registration.Customer.is_valid(invalid_customer_by_name)
 
     invalid_customer_by_name = %{valid_customer | name: "1bor"}
-    refute Customer.is_valid(invalid_customer_by_name)
+    refute Registration.Customer.is_valid(invalid_customer_by_name)
 
-
-    end
+  end
 
     test "invalid by aadhar", state do
       valid_customer = state[:valid_customer]
 
       invalid_customer_by_aadhar = %{valid_customer | aadhar: "abcsruchrt2d"}
-      refute Customer.is_valid(invalid_customer_by_aadhar)
+      refute Registration.Customer.is_valid(invalid_customer_by_aadhar)
 
       invalid_customer_by_aadhar = %{valid_customer | aadhar: "1234"}
-      refute Customer.is_valid(invalid_customer_by_aadhar)
-
+      refute Registration.Customer.is_valid(invalid_customer_by_aadhar)
 
       invalid_customer_by_aadhar = %{valid_customer | aadhar: "12345678901234"}
-      refute Customer.is_valid(invalid_customer_by_aadhar)
+      refute Registration.Customer.is_valid(invalid_customer_by_aadhar)
 
     end
-
 
 end
