dd775f219624741b96bf9792179b25f7ef619424
singlefleck <singlefleck@gmx.com>
2023-02-16 14:14:21 +0530

    Add new test file to validate customer details

---
+++ test/customer_test.exs
@@ -0,0 +1,50 @@
+defmodule CustomerTest do
+  use ExUnit.Case
+  doctest Customer
+
+  setup_all do
+    valid_customer = %{name: "boy", aadhar: "561664075050"}
+    invalid_customer= %{}
+
+    {:ok, valid_customer: valid_customer, invalid_customer: invalid_customer}
+
+
+  end
+
+  test "valid customer", state do
+    assert Customer.is_valid(state[:valid_customer])
+  end
+
+  test "invalid customer", state do
+    refute Customer.is_valid(state[:invalid_customer])
+  end
+
+  test "invalid by name", state do
+    valid_customer = state[:valid_customer]
+
+    invalid_customer_by_name = %{valid_customer | name: "ra"}
+    refute Customer.is_valid(invalid_customer_by_name)
+
+    invalid_customer_by_name = %{valid_customer | name: "1bor"}
+    refute Customer.is_valid(invalid_customer_by_name)
+
+
+    end
+
+    test "invalid by aadhar", state do
+      valid_customer = state[:valid_customer]
+
+      invalid_customer_by_aadhar = %{valid_customer | aadhar: "abcsruchrt2d"}
+      refute Customer.is_valid(invalid_customer_by_aadhar)
+
+      invalid_customer_by_aadhar = %{valid_customer | aadhar: "1234"}
+      refute Customer.is_valid(invalid_customer_by_aadhar)
+
+
+      invalid_customer_by_aadhar = %{valid_customer | aadhar: "12345678901234"}
+      refute Customer.is_valid(invalid_customer_by_aadhar)
+
+    end
+
+
+end
