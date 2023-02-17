39395b212497afd2256e6ff8063a4aee6258bc2a
Srujan Reddy <32371403+singlefleck@users.noreply.github.com>
2023-02-17 17:29:51 +0530

    Add new test file to validate customer details (#2)

---
+++ lib/registration/customer.ex
@@ -0,0 +1,36 @@
+defmodule Registration.Customer do
+  @spec is_valid(%{:aadhar => any, :name => binary, optional(any) => any}) :: nil | true
+
+  def is_valid(%{name: _name, aadhar: _aadhar} = customer_details) do
+    %{name: name, aadhar: aadhar} = customer_details
+
+    check_name(name) && check_aadhar(aadhar)
+  end
+
+  def is_valid(_any_customer_format) do
+    false
+  end
+
+  @spec check_name(binary) :: false | nil | true
+  def check_name(name) do
+    Regex.match?(~r/^([a-zA-Z]{3,30})/, name)
+  end
+
+  @spec check_aadhar(any) :: false | nil | true
+  def check_aadhar(aadhar) do
+    Regex.match?(~r/^([0-9]{12}$)/, aadhar)
+  end
+
+  def get_data_from_customer do
+    name = IO.gets("name with min 3'cha\n")
+    aadhar = IO.gets("enter your aadahr number\n")
+    customer_data = %{name: name, aadhar: aadhar}
+
+    if Registration.Customer.is_valid(customer_data) do
+      IO.puts("your KYC is successfully updated.")
+    else
+      IO.puts("Please check the details you have entered and try again!\n\n")
+      Registration.Customer.get_data_from_customer()
+    end
+  end
+end
---
+++ test/customer_test.exs
@@ -0,0 +1,41 @@
+defmodule Registration.CustomerTest do
+  use ExUnit.Case
+
+  setup_all do
+    valid_customer = %{name: "boy", aadhar: "561664075050"}
+    invalid_customer = %{}
+
+    {:ok, valid_customer: valid_customer, invalid_customer: invalid_customer}
+  end
+
+  test "valid customer", state do
+    assert Registration.Customer.is_valid(state[:valid_customer])
+  end
+
+  test "invalid customer", state do
+    refute Registration.Customer.is_valid(state[:invalid_customer])
+  end
+
+  test "invalid by name", state do
+    valid_customer = state[:valid_customer]
+
+    invalid_customer_by_name = %{valid_customer | name: "ra"}
+    refute Registration.Customer.is_valid(invalid_customer_by_name)
+
+    invalid_customer_by_name = %{valid_customer | name: "1bor"}
+    refute Registration.Customer.is_valid(invalid_customer_by_name)
+  end
+
+  test "invalid by aadhar", state do
+    valid_customer = state[:valid_customer]
+
+    invalid_customer_by_aadhar = %{valid_customer | aadhar: "abcsruchrt2d"}
+    refute Registration.Customer.is_valid(invalid_customer_by_aadhar)
+
+    invalid_customer_by_aadhar = %{valid_customer | aadhar: "1234"}
+    refute Registration.Customer.is_valid(invalid_customer_by_aadhar)
+
+    invalid_customer_by_aadhar = %{valid_customer | aadhar: "12345678901234"}
+    refute Registration.Customer.is_valid(invalid_customer_by_aadhar)
+  end
+end
