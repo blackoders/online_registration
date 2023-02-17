d1b301a34c901a4558f4e915da80e1ef42b8057e
singlefleck <singlefleck@gmx.com>
2023-02-17 14:26:32 +0530

    Add test cases for vehicle registration file

---
+++ test/vehicle_registration_test.exs
@@ -0,0 +1,31 @@
+defmodule Registration.VehicleTest do
+  use ExUnit.Case
+
+  setup_all do
+    valid_vehicle = %{
+      name: "sai",
+      aadhar: "561664075050",
+      vehicle_number: "TN 75 AA 7106",
+      maker_class: "TATA",
+      vehicle_class: "MOTOR CAR",
+      fuel_used: "PETROL",
+      type_of_body: "sedan"
+    }
+
+    invalid_vehicle = %{}
+    {:ok, valid_vehicle: valid_vehicle, invalid_vehicle: invalid_vehicle}
+  end
+
+  test "valid vehicle", state do
+    assert Registration.Vehicle.rc_generation(state[:valid_vehicle])
+  end
+
+  test "invalid vehicle", state do
+    refute Registration.vehicle().rc_generation(state[:invalid_vehicle])
+  end
+
+  test "invalid by fuel used", state do
+    valid_vehicle = state[:fuel_used]
+    refute Registration.vehicle().rc_generation(%{valid_vehicle | fuel_used: "air"})
+  end
+end
