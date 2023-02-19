82223e8da155d3827076e64d49d932f6690b919d
singlefleck <singlefleck@gmx.com>
2023-02-19 17:29:17 +0530

    Add Registraion helper module

---
+++ lib/helper.ex
@@ -0,0 +1,11 @@
+defmodule Registration.Helper do
+  def read_string(prompt) do
+    input = IO.gets(prompt)
+    String.trim(input)
+  end
+
+  def read_integer(prompt) do
+      input = IO.gets(prompt)
+      String.trim(input)
+  end
+end
